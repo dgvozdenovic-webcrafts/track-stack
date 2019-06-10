10623d677f170c64340bbe0474ab74b099af1a7d
Dejan Gvozdenovic <dejan.gvozdenovic@namics.com>
2019-06-10 11:18:24 +0200

    remove js and map files, firebase config

---
+++ firebase.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  credentials: {
+    apiKey: "AIzaSyA0wvNsqsw887CLcs7VQAXgjEZDN78OOTg",
+    authDomain: "track-stack-f8069.firebaseapp.com",
+    databaseURL: "https://track-stack-f8069.firebaseio.com",
+    projectId: "track-stack-f8069",
+    storageBucket: "track-stack-f8069.appspot.com",
+    messagingSenderId: "122779541361",
+    appId: "1:122779541361:web:73e0106a74e3df5d"
+  },
+  databaseUrl: 'https://track-stack-f8069.firebaseio.com/'
+} 
